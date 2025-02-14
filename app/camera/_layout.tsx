be9c938859a0b383740c361a22ada2cb99d5c575
Eugene Cross <eugene.cross@gmail.com>
2025-02-14 17:57:44 +0000

    feat: image view and picker added > preview page and nav

--- app/camera/_layout.tsx
+++ app/camera/_layout.tsx
@@ -1,19 +1,43 @@
 import { CameraType, CameraView, useCameraPermissions } from "expo-camera";
+import { Image } from "expo-image";
+import * as ImagePicker from "expo-image-picker";
+
 import { router } from "expo-router";
 
-import { useState } from "react";
-import { View, TouchableOpacity, Text, StyleSheet } from "react-native";
+import { useRef, useState } from "react";
+import { View, TouchableOpacity, Text, StyleSheet, Button } from "react-native";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
 
 export default function CameraLayout() {
+  const ref = useRef<CameraView>(null);
+  const [uri, setUri] = useState<string | null>(null);
+
   const [permission, requestPermission] = useCameraPermissions();
   const [facing, setFacing] = useState<CameraType>("back");
-  const [cameraOn, setCameraOn] = useState<boolean>(true);
 
   if (!permission) {
     return <View />;
   }
+
+  const takePhoto = async () => {
+    console.log("Take photo");
+
+    const photo = await ref?.current?.takePictureAsync();
+    console.log(photo);
+    if (photo) {
+      setUri(photo.uri);
+    }
+  };
+
+  const selectImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ["images", "videos"],
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+  };
 
   if (!permission?.granted) {
     return (
@@ -33,52 +57,76 @@ export default function CameraLayout() {
     );
   }
 
-  return (
-    <View style={styles.container}>
-      <CameraView style={styles.camera} facing={facing}>
-        <TouchableOpacity
-          style={styles.closeCameraButton}
-          onPress={() => {
-            router.back();
-          }}
-        >
-          <FontAwesome name="close" size={50} color="lightblue" />
-        </TouchableOpacity>
-        <View style={styles.buttonContainer}>
-          <TouchableOpacity
-            style={styles.button}
-            onPress={() => setFacing(facing === "back" ? "front" : "back")}
-          >
-            <Ionicons
-              name={facing === "back" ? "camera-reverse" : "camera"}
-              color="lightblue"
-              size={50}
-            />
+  const renderPicture = () => {
+    return (
+      <View style={styles.previewContainer}>
+        <Image
+          source={{ uri }}
+          contentFit="contain"
+          style={styles.previewImage}
+        ></Image>
+        <View style={styles.previewButtonContainer}>
+          <TouchableOpacity style={styles.button} onPress={() => setUri(null)}>
+            <Ionicons name={"camera"} color="lightblue" size={60} />
           </TouchableOpacity>
           <TouchableOpacity
             style={styles.button}
-            onPress={() => alert("Take a picture")}
+            onPress={() => alert("Save to camera roll")}
           >
-            <FontAwesome name="circle" size={64} color="lightblue" />
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.button}
-            onPress={() => alert("Go to photo library")}
-          >
-            <Ionicons name="images-outline" size={50} color="lightblue" />
+            <Ionicons name={"save-outline"} color="lightblue" size={60} />
           </TouchableOpacity>
         </View>
-      </CameraView>
+      </View>
+    );
+  };
+
+  const renderCamera = () => {
+    return (
+      <View style={styles.container}>
+        <CameraView ref={ref} style={styles.camera} facing={facing}>
+          <TouchableOpacity
+            style={styles.closeCameraButton}
+            onPress={() => {
+              router.back();
+            }}
+          >
+            <FontAwesome name="close" size={50} color="lightblue" />
+          </TouchableOpacity>
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity
+              style={styles.button}
+              onPress={() => setFacing(facing === "back" ? "front" : "back")}
+            >
+              <Ionicons
+                name={facing === "back" ? "camera-reverse" : "camera"}
+                color="lightblue"
+                size={50}
+              />
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.button} onPress={takePhoto}>
+              <FontAwesome name="circle" size={64} color="lightblue" />
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.button} onPress={selectImage}>
+              <Ionicons name="images-outline" size={50} color="lightblue" />
+            </TouchableOpacity>
+          </View>
+        </CameraView>
+      </View>
+    );
+  };
+
+  return (
+    <View style={styles.container}>
+      {uri ? renderPicture() : renderCamera()}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    marginTop: 50, // TODO safe area
+    marginTop: 30, // TODO safe area
     flex: 1,
-    // flex: 1,
-    // flexDirection: "column",
+
     justifyContent: "center",
   },
   message: {
@@ -123,5 +171,22 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: "bold",
     color: "lightblue",
+  },
+  previewContainer: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  previewImage: {
+    flex: 1,
+    width: "100%",
+    aspectRatio: 1,
+  },
+  previewButtonContainer: {
+    // flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginBottom: 10,
+    // backgroundColor: "transparent",
   },
 });
